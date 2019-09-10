a8eb0d29e7fe540960605710e16fcd9cf5593205
salmanyaqoobin <34335865+salmanyaqoobin@users.noreply.github.com>
2019-09-11 00:26:50 +0300

    Updates

---
+++ precache-manifest.713489b61b367b27fe763c42fb1728ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c15bd89c9e68ece74d3475208a1f9c02",
+    "url": "/react-monster-app/index.html"
+  },
+  {
+    "revision": "2e0e72c14bb397b84b6a",
+    "url": "/react-monster-app/static/css/main.1a18a0e0.chunk.css"
+  },
+  {
+    "revision": "d2554077cad495ef7c69",
+    "url": "/react-monster-app/static/js/2.3ecc8553.chunk.js"
+  },
+  {
+    "revision": "2e0e72c14bb397b84b6a",
+    "url": "/react-monster-app/static/js/main.f18594fa.chunk.js"
+  },
+  {
+    "revision": "88b2998c4f77fbe8eb3d",
+    "url": "/react-monster-app/static/js/runtime~main.dd30f927.js"
+  }
+]);
